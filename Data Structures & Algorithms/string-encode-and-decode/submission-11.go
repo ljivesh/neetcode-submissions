86dc705beec5c328610b295ec7267304edc1853f
Menma <96004270+ljivesh@users.noreply.github.com>
2026-07-27 21:55:48 +0530

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.go
@@ -0,0 +1,66 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+
+    sb := strings.Builder {}
+
+    for _, str := range strs {
+
+        size := len(str)
+
+        //append data in {size}#{string} format
+        sb.WriteString(strconv.Itoa(size))
+        sb.WriteByte('#')
+        sb.WriteString(str)
+
+    }
+
+    encoded := sb.String()
+
+    return encoded
+
+}
+
+func (s *Solution) Decode(encoded string) []string {
+
+   
+    decoded := make([]string, 0)
+
+    // Initalize two pointers
+    i := 0
+    j := 0
+
+    for i < len(encoded) {
+
+        j = i
+
+        //iterate till number end signal
+        for encoded[j] != byte('#') {
+            j++
+        }
+
+        //get size of str element
+        num := encoded[i:j]
+
+        //convert num string to int
+        convertedNum, err := strconv.Atoi(num)
+
+        if err != nil {
+            fmt.Println(err);
+            return []string {}
+        }
+
+        //set pointers to str element
+        i = j+1
+        j = i+convertedNum
+
+        str := encoded[i:j]
+
+        decoded = append(decoded, str)
+
+        i = j
+        
+    }
+
+    return decoded
+}
