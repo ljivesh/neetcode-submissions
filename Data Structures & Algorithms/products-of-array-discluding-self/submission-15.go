0edc9b03caf0848f8ada0e590bd05dd3dfbd80aa
Menma <96004270+ljivesh@users.noreply.github.com>
2026-07-28 07:28:00 +0530

    Add: products-of-array-discluding-self - submission-15

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-15.go
@@ -0,0 +1,27 @@
+func productExceptSelf(nums []int) []int {
+
+    prefix := 1
+
+    output := make([]int, len(nums))
+
+    for i:=0; i<len(nums); i++ {
+        
+        output[i] = prefix 
+
+        prefix = nums[i]*prefix
+
+    }
+
+    suffix := 1;
+
+    for i:=len(nums)-1; i>=0; i-- {
+
+        output[i] = output[i] * suffix
+
+        suffix = suffix * nums[i]        
+
+    }
+
+    return output
+
+}
