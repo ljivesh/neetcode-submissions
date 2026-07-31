562e71a36d527e9f6e1d44f1a6808d5350602b3e
Menma <96004270+ljivesh@users.noreply.github.com>
2026-07-31 11:59:11 +0530

    Add: majority-element - submission-9

---
+++ Data Structures & Algorithms/majority-element/submission-9.go
@@ -0,0 +1,23 @@
+func majorityElement(nums []int) int {
+	
+	frequencyMap := make(map[int]int)
+
+
+	for _,elem := range nums {
+		frequencyMap[elem]++
+	}
+
+	majorityElement := 0
+
+	for key, value := range frequencyMap {
+		if value >= (len(nums)/2)+1 {
+			majorityElement = key
+			break
+		}
+
+	}
+
+	return majorityElement
+
+	
+}
